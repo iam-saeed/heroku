7c94e106085b994561a1b50990092a36c4d247df
Saeed Khan <saeedkhan@ghost.hsd1.co.comcast.net>
2021-06-10 16:42:07 -0600

    monolith push

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 9000
+const path = require("path")
 
 app.use(cors())
 
 app.use(express.json())
-
+app.use(express.static(path.join(__dirname, "client/build")))
 app.use("/api/", (_, res) => {
     res.json({
         data: "The API is serving data!!"
@@ -16,6 +17,9 @@ app.use("/api/", (_, res) => {
 
 app.listen(port, () => {
     console.log(`Server is alive on port ${port}`)
+})
+app.use("*",(_,res)=>{
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
 })
 
 console.log("It is alive!")
